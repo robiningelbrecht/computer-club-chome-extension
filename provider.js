14e9182796a07c12c79aba393c0bb145b8cc54ad
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-11-22 16:11:24 +0100

    Designing is my thang now

--- provider.js
+++ provider.js
@@ -6,14 +6,15 @@ const json = await response.json();
 
 // https://computerclub.mailcoach.app/feed/ad8001d0-bb6b-48d6-ba7d-eecbff10e010
 
-const response2 = await fetch('https://computerclub.online/afleveringen/?format=rss', {
+const rssResponse = await fetch('https://computerclub.online/afleveringen/?format=rss', {
     method: "GET",
     cache: "no-cache",
 });
-const rss = new window.DOMParser().parseFromString(await response2.text(), "text/xml");
+const rss = new window.DOMParser().parseFromString(await rssResponse.text(), "text/xml");
 rss.querySelectorAll("item").forEach(el => {
     const title = el.querySelector("title").innerHTML;
-    const url = el.querySelector("link").innerHTML;
+    const link = el.querySelector("link").innerHTML;
     const description = el.querySelector("description").innerHTML;
     const pubDate = el.querySelector("pubDate").innerHTML;
+    const image = el.querySelector("content").getAttribute('url');
 });
